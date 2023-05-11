50534adeed5994706fcb5cde1c30f1e5dc610483
ahapsin <121087803+ahapsin@users.noreply.github.com>
2023-05-11 09:34:01 +0700

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,137 @@
-import Image from 'next/image'
+"use client";
+import { Button, Dropdown, Link, Navbar, Text } from "@nextui-org/react";
+import { IconAccessibleOffFilled } from "@tabler/icons-react";
 
 export default function Home() {
+  const collapseItems = [
+    "Features",
+    "Customers",
+    "Pricing",
+    "Company",
+    "Legal",
+    "Team",
+    "Help & Feedback",
+    "Login",
+    "Sign Up",
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Navbar shouldHideOnScroll variant="sticky">
+      <Navbar.Brand>
+        <Navbar.Toggle aria-label="toggle navigation" showIn={"xs"} />
+        <Text b color="inherit" hideIn="xs">
+          ACME
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content
+        enableCursorHighlight
+        activeColor="primary"
+        hideIn="xs"
+        variant="underline"
+      >
+        <Dropdown isBordered>
+          <Navbar.Item>
+            <Dropdown.Button
+              auto
+              light
+              css={{
+                px: 0,
+                dflex: "center",
+                svg: { pe: "none" },
+              }}
+              ripple={false}
+            >
+              Features
+            </Dropdown.Button>
+          </Navbar.Item>
+          <Dropdown.Menu
+            aria-label="ACME features"
+            css={{
+              $$dropdownMenuWidth: "340px",
+              $$dropdownItemHeight: "70px",
+              "& .nextui-dropdown-item": {
+                py: "$4",
+                // dropdown item left icon
+                svg: {
+                  color: "$primary",
+                  mr: "$4",
+                },
+                // dropdown item title
+                "& .nextui-dropdown-item-content": {
+                  w: "100%",
+                  fontWeight: "$semibold",
+                },
+              },
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <Dropdown.Item
+              key="autoscaling"
+              showFullDescription
+              description="ACME scales apps to meet user demand, automagically, based on load."
+              icon={<IconAccessibleOffFilled />}
+            >
+              Autoscaling
+            </Dropdown.Item>
+            <Dropdown.Item
+              key="usage_metrics"
+              showFullDescription
+              description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
+            >
+              Usage Metrics
+            </Dropdown.Item>
+            <Dropdown.Item
+              key="production_ready"
+              showFullDescription
+              description="ACME runs on ACME, join us and others serving requests at web scale."
+            >
+              Production Ready
+            </Dropdown.Item>
+            <Dropdown.Item
+              key="99_uptime"
+              showFullDescription
+              description="Applications stay on the grid with high availability and high uptime guarantees."
+            >
+              +99% Uptime
+            </Dropdown.Item>
+            <Dropdown.Item
+              key="supreme_support"
+              showFullDescription
+              description="Overcome any challenge with a supporting team ready to respond."
+            >
+              +Supreme Support
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Navbar.Link isActive href="#">
+          Customers
+        </Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="inherit" href="#">
+          Login
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Sign Up
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+      <Navbar.Collapse>
+        {collapseItems.map((item, index) => (
+          <Navbar.CollapseItem key={item}>
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href="#"
+            >
+              {item}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
