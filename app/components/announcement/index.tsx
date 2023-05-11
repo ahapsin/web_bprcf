2738669c0a7f416157b3a88d4994fe12e9da9672
ahapsin <121087803+ahapsin@users.noreply.github.com>
2023-05-11 10:44:06 +0700

    layout

---
+++ app/components/announcement/index.tsx
@@ -0,0 +1,14 @@
+const Announcement = () => {
+  return (
+    <div className="bg-gradient-to-r from-blue-500 via-yellow-500 to-red-500 px-4 py-3 text-white">
+      <p className="text-center text-sm font-medium">
+        Butuh Bantuan ?
+        <a href="#" className="inline-block underline">
+          Check out this new course!
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Announcement;
